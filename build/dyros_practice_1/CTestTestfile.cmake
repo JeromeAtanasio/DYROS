ddb0039935f0c7ef00473eda85b6fbe8a22a7ba0
Jerome <jeromeatanasio@gmail.com>
2022-07-06 09:01:38 +0900

    first commit

---
+++ build/dyros_practice_1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotv/catkin_ws/src/dyros_practice_1
+# Build directory: /home/robotv/catkin_ws/build/dyros_practice_1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
